375b1a4108c5db059ac7900be52a0d4fa03990e7
Akansh Garg <garg.akansh@gmail.com>
2020-09-06 15:07:43 +0530

    adding feature file for post request

--- src/test/java/com/karate/runner/TestRunner.java
+++ src/test/java/com/karate/runner/TestRunner.java
@@ -7,7 +7,7 @@ import com.intuit.karate.junit4.Karate;
 
 @RunWith(Karate.class)
 //@KarateOptions(features = {"classpath:featurefiles/basicinfo/userDetails.feature"})
-@KarateOptions(features = {"classpath:featurefiles/featurewithinfeaturefile/userdetails1.feature"})
+@KarateOptions(features = {"classpath:featurefiles/postrequestexample/postrequestdetails.feature"})
 public class TestRunner {
 
 }
