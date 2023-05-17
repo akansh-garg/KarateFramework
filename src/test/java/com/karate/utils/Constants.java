208221b10ae0391287a57f6aa0d568bcb5a40609
Akansh Garg <akansh.garg@bold.com>
2023-05-17 21:57:27 +0530

    deleting old framework

--- src/test/java/com/karate/utils/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.karate.utils;
-
-import java.io.File;
-
-public class Constants {
-
-	public final static String REPORTING_DIRECTORY = System.getProperty("user.dir") + File.separator + "Reporting";
-	
-}
